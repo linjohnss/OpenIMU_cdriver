17f05e8068194878fc5adf20baca9a9be486ff62
linjohnss <linjohn0903@gmail.com>
2022-03-11 14:53:41 +0800

    Fix OpenIMU 330 output result form int_32 to float

--- imu_driver.c
+++ imu_driver.c
@@ -18,35 +18,56 @@ u_int32_t reverse(u_int32_t x)
     u_int32_t n = x;
     n = ((n & 0xffff0000) >> 16) | ((n & 0x0000ffff) << 16);
     n = ((n & 0xff00ff00) >>  8) | ((n & 0x00ff00ff) <<  8);
-    //n = ((n & 0xf0f0f0f0) >>  4) | ((n & 0x0f0f0f0f) <<  4);
-    //n = ((n & 0xcccccccc) >>  2) | ((n & 0x33333333) <<  2);
-    //n = ((n & 0xaaaaaaaa) >>  1) | ((n & 0x55555555) <<  1);
     return n;
 } 
 
-void parse_data_383(int16_t *data)
+typedef struct imuData *imuDataPointer;
+struct imuData
 {
-    printf("%f ", (float)data[0]*20/(1<<16));
-    printf("%f ", (float)data[1]*20/(1<<16));
-    printf("%f ", (float)data[2]*20/(1<<16));
-    printf("%f ", (float)data[3]*1260/(1<<16));
-    printf("%f ", (float)data[4]*1260/(1<<16));
-    printf("%f ", (float)data[5]*1260/(1<<16));
-    printf("%f ", (float)data[6]*200/(1<<16));
-    printf("%f ", (float)data[7]*200/(1<<16));
-    printf("%f ", (float)data[8]*200/(1<<16));
-    printf("%f ", (float)data[9]*200/(1<<16));
-    printf("%hu ", (data[10]));
+    union time { u_int16_t t_383; u_int32_t t_330; } time;
+    float accx;
+    float accy;
+    float accz;
+    float gyrox;
+    float gyroy;
+    float gyroz;
+};
+
+void parse_data_383(int16_t *data, imuDataPointer result)
+{
+    result->time.t_383 = data[10];
+    result->accx = (float)data[0]*20/(1<<16);
+    result->accy = (float)data[1]*20/(1<<16);
+    result->accz = (float)data[2]*20/(1<<16);
+    result->gyrox = (float)data[3]*1260/(1<<16);
+    result->gyroy = (float)data[4]*1260/(1<<16); 
+    result->gyroz = (float)data[5]*1260/(1<<16);    
+    printf("%f ", result->accx);
+    printf("%f ", result->accy);
+    printf("%f ", result->accz);
+    printf("%f ", result->gyrox);
+    printf("%f ", result->gyroy);
+    printf("%f ", result->gyroz);
+    printf("%hu ", result->time.t_383);
     return;
 }
 
-void parse_data_330(int32_t *data)
+void parse_data_330(int32_t *data, imuDataPointer result)
 {
-    printf("%X ", data[0]);
-    printf("%f ", (float)data[1]);
-    printf("%f ", (float)data[2]);
-    printf("%f ", (float)data[3]);
-
+    result->time.t_330 = data[0];
+    result->accx = *((float*)((void*)(&data[1])));
+    result->accy = *((float*)((void*)(&data[2])));
+    result->accz = *((float*)((void*)(&data[3])));
+    result->gyrox = *((float*)((void*)(&data[4])));
+    result->gyroy = *((float*)((void*)(&data[5]))); 
+    result->gyroz = *((float*)((void*)(&data[6])));    
+    printf("%f ", result->accx);
+    printf("%f ", result->accy);
+    printf("%f ", result->accz);
+    printf("%f ", result->gyrox);
+    printf("%f ", result->gyroy);
+    printf("%f ", result->gyroz);
+    printf("%u ", result->time.t_330);
     return;
 }
 
@@ -80,6 +101,9 @@ int main(int argc, int **argv) {
     int8_t head;
     int16_t p_type;
     int8_t length = 0;
+    imuDataPointer result;
+    result = (imuDataPointer)malloc(sizeof(imuDataPointer*));
+    
     int n = 0;
     while(1) {
         while((n = read(serial_port, &head, sizeof(head))) > 0) {
@@ -92,18 +116,18 @@ int main(int argc, int **argv) {
                                 int16_t buffer[12];
                                 if(n = read(serial_port, &buffer, sizeof(buffer)) > 0) {
                                     ptr = buffer;
-                                    parse_data_383(&(*ptr));
+                                    parse_data_383(&(*ptr), &(*result));
                                     printf("\n");
                                 }
-                                free(ptr);
+                                memset(result, 0, sizeof(result));
                             } else if(p_type == PACKET_TYPE_330) {
                                 if(n = read(serial_port, &length, sizeof(length)) > 0) {
                                     int32_t *buffer = (int32_t*)malloc((length/4) * sizeof(int32_t));
                                     if(n = read(serial_port, &(*buffer), (length/4) * sizeof(int32_t)) > 0) {
-                                        parse_data_330(&(*buffer));
+                                        parse_data_330(&(*buffer), &(*result));
                                         printf("\n");
                                     }
-                                    free(buffer);
+                                    memset(result, 0, sizeof(result));
                                 }
                             }
                         } 
@@ -113,5 +137,6 @@ int main(int argc, int **argv) {
             }
         }
     }
+    free(result);
     return 0;
 }
