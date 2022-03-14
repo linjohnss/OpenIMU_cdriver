8dfc9bf403a46b2aa5f56678fd22ecb4063654c4
linjohnss <linjohn0903@gmail.com>
2022-03-14 14:44:13 +0800

    Fix OpenIMU 383 input format
    OpenIMU 383 have length input, so we have to handle it.
    
    After handle length input, the payload data have to reverse.

--- imu_driver.c
+++ imu_driver.c
@@ -12,10 +12,17 @@
 # define PACKET_TYPE_383 0x3153
 # define PACKET_TYPE_330 0x317A
 
+int16_t
+reverse(int16_t x)
+{
+    x = (((x & 0xff00) >> 8) | ((x & 0x00ff) << 8));
+    return x;
+}
+
 typedef struct imuData *imuDataPointer;
 struct imuData
 {
-    union time { u_int16_t t_383; u_int32_t t_330; } time;
+    union time { float t_383; u_int32_t t_330; } time;
     float accx;
     float accy;
     float accz;
@@ -24,26 +31,29 @@ struct imuData
     float gyroz;
 };
 
+float hint_to_float(u_int16_t input)
+{
+    float exp = 0.0;
+    float frac = 0.0;
+
+} 
+
 void parse_data_383(int16_t *data, imuDataPointer result)
 {
-    result->time.t_383 = data[10];
-    result->accx = (float)data[0]*20/(1<<16);
-    result->accy = (float)data[1]*20/(1<<16);
-    result->accz = (float)data[2]*20/(1<<16);
-    result->gyrox = (float)(data[3])*20/(1<<16);
-    result->gyroy = (float)(data[4])*20/(1<<16); 
-    result->gyroz = (float)(data[5])*20/(1<<16);    
+    result->time.t_383 = (float)(u_int16_t)reverse(data[10])*15.259022;
+    result->accx = (float)reverse(data[0])*20/(1<<16);
+    result->accy = (float)reverse(data[1])*20/(1<<16);
+    result->accz = (float)reverse(data[2])*20/(1<<16);
+    result->gyrox = (float)reverse(data[3])*1260/(1<<16);
+    result->gyroy = (float)reverse(data[4])*1260/(1<<16); 
+    result->gyroz = (float)reverse(data[5])*1260/(1<<16);    
     printf("%f ", result->accx);
     printf("%f ", result->accy);
     printf("%f ", result->accz);
     printf("%f ", result->gyrox);
     printf("%f ", result->gyroy);
     printf("%f ", result->gyroz);
-    printf("%hu ", result->time.t_383);
-    
-    // printf("%X ", data[3]);
-    // printf("%X ", data[4]);
-    // printf("%X ", data[5]);
+    printf("%f ", result->time.t_383);
     return;
 }
 
@@ -67,7 +77,7 @@ void parse_data_330(int32_t *data, imuDataPointer result)
 }
 
 int main(int argc, int **argv) {
-    int serial_port = open("/dev/ttyUSB0", O_RDWR);
+    int serial_port = open("/dev/ttyUSB1", O_RDWR);
 
     if (serial_port <0) {
         printf("Error %i from open: %s\n", errno, strerror(errno));
@@ -98,24 +108,23 @@ int main(int argc, int **argv) {
     int8_t length = 0;
     imuDataPointer result;
     result = (imuDataPointer)malloc(sizeof(imuDataPointer*));
-    int8_t data[40];
+    int8_t data[31];
     int n = 0;
     while(1) {
         while((n = read(serial_port, &head, sizeof(head))) > 0) {
             if(head == HEADER) {
                 if(n = read(serial_port, &head, sizeof(head)) > 0) {
                     if(head == HEADER) {
-                        //puts("get header");
                         if(n = read(serial_port, &p_type, sizeof(p_type)) > 0) {
                             if(p_type == PACKET_TYPE_383) {
-                                int16_t *ptr;
-                                int16_t buffer[12];
-                                if(n = read(serial_port, &buffer, sizeof(buffer)) > 0) {
-                                    ptr = buffer;
-                                    parse_data_383(&(*ptr), &(*result));
-                                    printf("\n");
+                                if(n = read(serial_port, &length, sizeof(length)) > 0) {
+                                    int16_t *buffer = (int16_t*)malloc((length/2) * sizeof(int16_t));
+                                    if(n = read(serial_port, &(*buffer), (length/2) * sizeof(int16_t)) > 0) {
+                                        parse_data_383(&(*buffer), &(*result));
+                                        printf("\n");
+                                    }
+                                    memset(result, 0, sizeof(result));
                                 }
-                                memset(result, 0, sizeof(result));
                             } else if(p_type == PACKET_TYPE_330) {
                                 if(n = read(serial_port, &length, sizeof(length)) > 0) {
                                     int32_t *buffer = (int32_t*)malloc((length/4) * sizeof(int32_t));
@@ -131,11 +140,6 @@ int main(int argc, int **argv) {
                 }
             }
         }
-        // while((n = read(serial_port, &data, sizeof(data))) > 0) {
-        //     for(int i=0;i<40;i++)
-        //         printf("%hX ", data[i]);
-        //     printf("\n");
-        // }
     }
     free(result);
     return 0;
